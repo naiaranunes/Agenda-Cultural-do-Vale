df1c9dc01559df0a0c8ad8d96d11167cc32810d3
naiara.nunes <naiara.nunes@aluno.ifsertao-pe.edu.br>
2026-05-26 22:28:22 -0300

    cirando login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
-
-from django.http import HttpResponse
-
 def login_usuario(request):
-    return HttpResponse('pagina de login')
+    return render(request, 'usuarios/login.html')
+
