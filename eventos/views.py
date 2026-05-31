92ea47f0f30828d6283d943f17898173cc0f6d3d
naiara.nunes <naiara.nunes@aluno.ifsertao-pe.edu.br>
2026-05-31 14:10:39 -0300

    melhorando arquivos

--- eventos/views.py
+++ eventos/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def home(request):
-     return render(request, 'home.html'),   
+    return render(request, 'home.html')
 
 def cadastrar_evento(request):
      return render(request, 'eventos/cadastrar-evento.html')
