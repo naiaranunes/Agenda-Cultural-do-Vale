baf745264ce4460259f10b900d11fc8a57b6d2fb
naiara.nunes <naiara.nunes@aluno.ifsertao-pe.edu.br>
2026-05-31 17:11:28 -0300

    cadstro usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_usuario),
+    path('cadastro/', views.cadastro_login), 
+
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 # Create your views here.
+#cadastro de usuários
+def cadastro_login(request):
+    return render(request, 'usuarios/cadastro-usuario.html')
+
 def login_usuario(request):
     return render(request, 'usuarios/login.html')
 
